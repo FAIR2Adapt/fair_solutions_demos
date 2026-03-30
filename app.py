c62438d987cd8fa13e8d94b11ddb4e9136a0b753
esgg <eg.guardia@gmail.com>
2026-03-30 18:19:20 +0200

    New page added

--- app.py
+++ app.py
@@ -20,10 +20,10 @@ st.sidebar.title("Menu")
 if st.sidebar.button("🏠 Overview", use_container_width=True):
     st.session_state["selected_page"] = "Overview"
 
-"""
+
 if st.sidebar.button("1️⃣ FAIR Solution #1", use_container_width=True):
     st.session_state["selected_page"] = "FAIR Solution #1"
-"""
+
 if st.sidebar.button("2️⃣ FAIR Solution #2", use_container_width=True):
     st.session_state["selected_page"] = "FAIR Solution #2"
 
@@ -37,11 +37,11 @@ page = st.session_state["selected_page"]
 
 if page == "Overview":
     overview()
-elif page == "FAIR Solution #1":
-    page_fs_1()
-elif page == "FAIR Solution #2":
-    page_fs_2()
-elif page == "FAIR Solution #3":
-    page_fs_3()
+#elif page == "FAIR Solution #1":
+#    page_fs_1()
+#elif page == "FAIR Solution #2":
+#    page_fs_2()
+#elif page == "FAIR Solution #3":
+#    page_fs_3()
 elif page == "FAIR Solution #4":
     page_fs_4()
