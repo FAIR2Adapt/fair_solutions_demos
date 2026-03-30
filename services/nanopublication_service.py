674000de80ebec89d1d12a4d2f67353cbd39b888
esgg <eg.guardia@gmail.com>
2026-03-30 18:21:27 +0200

    New variable added in state

--- services/nanopublication_service.py
+++ services/nanopublication_service.py
@@ -2,11 +2,13 @@ from rdflib import Dataset, Namespace, URIRef, Literal
 from rdflib.namespace import RDF, RDFS, XSD, FOAF
 from datetime import datetime, timezone
 import urllib.parse
+import nanopub
+from nanopub import Nanopub, NanopubConf, load_profile
+from pathlib import Path
 
 PUBLISHER = "https://sciencelive4all.org/"
 
 # Namespace to publish. We can use our own namespace
-
 TEMP_NP = Namespace("https://w3id.org/np/temp") 
 
 TEMP_NP = Namespace("https://w3id.org/sciencelive/np") 
@@ -35,6 +37,8 @@ PUBINFO_TEMPLATE_2 = URIRef("https://w3id.org/np/RAukAcWHRDlkqxk7H2XNSegc1WnHI56
 HYCL_AIDA_SENTENCE = URIRef("http://purl.org/petapico/o/hycl#AIDA-Sentence")
 HYCL_NS = URIRef("http://purl.org/petapico/o/hycl")
 
+profile = load_profile("./profile.yml")
+print(f"Loaded profile: \n{profile.name}\n{profile.orcid_id}")
 
 def create_aida_nanopub(logger, claim):
     """
@@ -48,14 +52,24 @@ def create_aida_nanopub(logger, claim):
         tuple: (Dataset, label)
     """
     # CRITICAL: Use temporary namespace that gets replaced when signing
+
+    logger.info("Creating nanopublication")
     
+    creator_orcid = "https://orcid.org/0000-0003-4112-6825"
+    creator_name = "Esteban Gonzalez"
+    AUTHOR_ORCID = creator_orcid
+    AUTHOR_NAME = creator_name
+
+
     this_np = URIRef(TEMP_NP)
     head_graph = URIRef(TEMP_NP + "/Head")
     assertion_graph = URIRef(TEMP_NP + "/assertion")
     provenance_graph = URIRef(TEMP_NP + "/provenance")
     pubinfo_graph = URIRef(TEMP_NP + "/pubinfo")
 
-    
+    author_uri = URIRef(creator_orcid)
+
+
     # Create AIDA sentence URI (URL-encoded)
     aida_sentence = claim["claim"]
     logger.info("Add claim: "+str(aida_sentence))
@@ -92,12 +106,18 @@ def create_aida_nanopub(logger, claim):
     assertion = ds.graph(assertion_graph)
     assertion.add((aida_uri, RDF.type, HYCL_AIDA_SENTENCE))
     
+     # PROVENANCE graph
+    provenance = ds.graph(provenance_graph)
+    provenance.add((assertion_graph, PROV.wasAttributedTo, author_uri))
+    
    
     # PUBINFO graph
     pubinfo = ds.graph(pubinfo_graph)
+    pubinfo.add((author_uri, FOAF.name, Literal(creator_name)))
     
     now = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.000Z")
     pubinfo.add((this_np, DCT.created, Literal(now, datatype=XSD.dateTime)))
+    pubinfo.add((this_np, DCT.creator, author_uri))
     pubinfo.add((this_np, DCT.license, URIRef("https://creativecommons.org/licenses/by/4.0/")))
     pubinfo.add((this_np, NPX.wasCreatedAt, URIRef(PUBLISHER)))
     
@@ -120,3 +140,39 @@ def create_aida_nanopub(logger, claim):
     pubinfo.add((this_np, NT.wasCreatedFromPubinfoTemplate, PUBINFO_TEMPLATE_2))
     
     return ds, label
+
+
+
+def publish_nanopublication2(logger, nanopub_data:dict):
+
+    logger.info(nanopub_data)
+
+    logger.info("Nanopublication:"+nanopub_data["label"])
+
+    with open("example.trig", 'r') as f:
+        print(f.read())
+
+    conf = NanopubConf(profile=profile)
+
+    #trig_text = Path("example.trig").read_text(encoding="utf-8")
+
+    np_obj = Nanopub(rdf="example_anne.trig", conf=conf)
+    np_obj.sign()
+        
+    np_obj.publish()
+    print(f"✓ Published: {np_obj.source_uri}")
+
+def publish_nanopublication(logger, nanopub_data:dict):
+
+    logger.info(nanopub_data)
+
+    logger.info("Nanopublication:"+nanopub_data["label"])
+
+    conf = NanopubConf(profile=profile)
+
+    np_obj = Nanopub(rdf=nanopub_data["nanopublication"], conf=conf)
+    np_obj.check()
+    np_obj.sign()
+        
+    np_obj.publish()
+    print(f"✓ Published: {np_obj.source_uri}")
