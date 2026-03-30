d4a1726f28776352cec266aa1409252a7593e38b
esgg <eg.guardia@gmail.com>
2026-03-30 18:24:04 +0200

    New workflow added

--- views/page_fs_4.py
+++ views/page_fs_4.py
@@ -2,13 +2,15 @@ import streamlit as st
 
 from utils.logging_config import setup_logger
 from steps.fair_publication.step_1_fair_object import render_step_1
-from steps.fair_publication.step_2_resource_source import render_step_2
-from steps.fair_publication.step_3_doi import render_step_3_doi
+from steps.fair_publication.step_2_resource_source import render_step_select_resource
+from steps.fair_publication.step_3_doi import render_step_doi
 from steps.fair_publication.step_3_pdf import render_step_3_pdf
 from steps.fair_publication.step_4_claims import render_step_4
 from steps.fair_publication.step_5_nanopublication import render_step_5
 from steps.fair_publication.step_6_enrichment import render_step_6
 from steps.fair_publication.step_7_fdo import render_step_7
+from steps.fair_publication.step_add_metadata_manually import render_add_metadata_manually
+
 
 logger = setup_logger("app")
 
@@ -16,12 +18,12 @@ logger = setup_logger("app")
 def render_publication_flow() -> None:
     st.success("You selected: FAIRify Publication")
 
-    render_step_2()
+    render_step_select_resource()
 
     resource_type = st.session_state.get("publication_resource_type")
 
     if resource_type == "doi":
-        render_step_3_doi(logger)
+        render_step_doi(logger)
 
         doi_result = st.session_state.get("doi_result")
         if doi_result and "error" not in doi_result:
@@ -45,7 +47,51 @@ def render_publication_flow() -> None:
                 if enrichment_result and "error" not in enrichment_result:
                     st.divider()
                     render_step_7(logger)
+                elif enrichment_result and "error" in enrichment_result:
+                    st.divider()
+                    render_step_7(logger)
 
+        elif doi_result and "error"  in doi_result:
+            
+            render_add_metadata_manually(logger)
+            
+            doi_result = st.session_state.get("doi_result")
+
+            if doi_result and "error" not in doi_result:
+                st.divider()
+                logger.info("STEP 4 enter")
+                render_step_4(logger)
+
+                claims_result = st.session_state.get("claims_result")
+                logger.info("STEP 4 exit")
+
+                logger.info(claims_result)
+
+                if claims_result and "error" not in claims_result:
+                    st.divider()
+                    logger.info("Enter to claim extraction process")
+                    render_step_5(logger)
+
+                    nanopublications_result = st.session_state.get("nanopublications_result")
+
+                    if nanopublications_result and "error" not in nanopublications_result:
+                        st.divider()
+                        render_step_6(logger)
+
+                    enrichment_result = st.session_state.get("enrichment_result")
+
+                    if enrichment_result and "error" not in enrichment_result:
+                        st.divider()
+                        render_step_7(logger)
+                elif claims_result and "error" in claims_result:
+                    logger.info("Skip claims extraction process")
+                    render_step_6(logger)
+
+                    enrichment_result = st.session_state.get("enrichment_result")
+
+                    if enrichment_result and "error" not in enrichment_result:
+                        st.divider()
+                        render_step_7(logger)
     elif resource_type == "pdf":
         render_step_3_pdf()
 
