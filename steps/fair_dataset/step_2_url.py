e745ffd01b9731abcc280af97816f3682c58ef6e
esgg <eg.guardia@gmail.com>
2026-06-29 08:31:01 +0200

    Component to provide a url to explore, and execute the workflow to extract metadata

---
+++ steps/fair_dataset/step_2_url.py
@@ -0,0 +1,72 @@
+import streamlit as st
+from services.seanoe_service import extract_seanoe_metadata
+from services.seanoe_service import extract_seanoe_metadata_scrapping
+from services.sextant_service import extract_sextant_metadata
+from services.doi_service import fetch_doi_metadata
+from utils.fair_publication_state import split_abstract
+
+def render_step_url(logger) -> None:
+    st.subheader("Step: Enter a URL from one of the databases")
+
+    st.text_input(
+        "URL",
+        placeholder="e.g. https://www.seanoe.org/data/01068/118008/",
+        key="url_value",
+    )
+
+    if st.button("Process URL", use_container_width=True):
+        url = st.session_state.get("url_value", "").strip()
+        st.session_state["url"] = url
+
+        logger.info("Processing URL: %s", url)
+
+        with st.spinner("Retrieving metadata..."):
+            try:
+                if "seanoe" in url:
+                    result = extract_seanoe_metadata_scrapping(logger, url)
+                elif "sextant" in url:
+                    result = extract_sextant_metadata(logger, url)
+
+                logger.info("Metadata")
+                logger.info(result)
+                result_doi = fetch_doi_metadata(result["doi"])
+
+                result["abstract"] = result_doi["abstract"]
+                if "sextant" in url:
+                    result["title"] = result_doi["title"]
+                    url_value = f"https://doi.org/{result["doi"]}"
+                    result["files_involved"].append({"name":result["title"],"url":url_value})
+                st.session_state["doi_result"] = result
+                st.session_state.pop("claims_result", None)
+                logger.info("URL metadata retrieved successfully")
+                logger.info(result)
+            except Exception as exc:
+                    logger.exception("DOI processing failed")
+                    st.session_state["doi_result"] = {"error": str(exc)}
+
+    url_result = st.session_state.get("doi_result")
+
+    if not url_result:
+        return
+
+    if "error" in url_result:
+        st.error("Problem to retrive metadata from DOI")
+        return
+
+    st.success(f"Metadata retrieved from {url_result['url']}")
+
+    st.subheader("Title")
+    st.write(url_result.get("title", "No title found"))
+
+    st.subheader("Abstract")
+    abstract = url_result.get("abstract")
+
+    if abstract:
+        preview, remainder = split_abstract(abstract, max_chars=300)
+        st.write(preview)
+
+        if remainder:
+            with st.expander("Show remaining abstract"):
+                st.write(remainder)
+    else:
+        st.info("No abstract available.")
